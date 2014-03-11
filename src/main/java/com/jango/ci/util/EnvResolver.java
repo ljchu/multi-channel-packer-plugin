d62615ae1cee93d42fc0d73abcc1f1f88f890013
ljchu <aljchu@163.com>
2014-03-11 20:49:01 +0800

    Some changes happen.

---
+++ src/main/java/com/jango/ci/util/EnvResolver.java
@@ -0,0 +1,52 @@
+package com.jango.ci.util;
+
+import java.util.Map;
+
+public class EnvResolver {
+
+	private static String left = "${";
+	private static String right = "}";
+
+	public static String getLeft() {
+		return left;
+	}
+
+	public static void setLeft(String left) {
+		EnvResolver.left = left;
+	}
+
+	public static String getRight() {
+		return right;
+	}
+
+	public static void setRight(String right) {
+		EnvResolver.right = right;
+	}
+
+	public static String changeStringWithEnv(Map<?, ?> map, String inputString) {
+		StringBuffer aBuffer = new StringBuffer(inputString);
+		int aIndex = -1;
+		int bIndex = -1;
+		StringBuffer bf = null;
+		while ((aIndex = aBuffer.indexOf(left)) != -1
+				&& ((bIndex = aBuffer.indexOf(right)) != -1)) {
+			boolean a = map.containsKey(aBuffer.substring(aIndex + 2, bIndex));
+			if (!a) {
+				break;
+			} else {
+				bf = aBuffer
+						.replace(aIndex, bIndex + 1, (String) map.get(aBuffer
+								.substring(aIndex + 2, bIndex)));
+				aBuffer = bf;
+				aIndex = aBuffer.indexOf(left);
+				bIndex = aBuffer.indexOf(right);
+			}
+		}
+
+		if (bf != null) {
+			return bf.toString();
+		} else {
+			return inputString;
+		}
+	}
+}
